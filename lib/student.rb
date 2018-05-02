910e7b0bb0ce053f905f868dc51ac022ba39aa83
github username <jeffreyhwang90@gmail.com>
2018-05-01 23:18:43 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,56 @@
+require 'pry'
+
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def id
+    DB[:conn].execute("SELECT id FROM students WHERE name = ?", self.name)[0][0]
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, @name, @grade)
+    # binding.pry
+    self.id
+    # binding.pry
+  end
+
+  def self.create(hash)
+    student = Student.new(hash[:name], hash[:grade])
+    student.save
+    student
+  end
+    # bindinggrade
 end
